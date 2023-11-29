f3b691f60802451c4be5b08a797ded9c0a7d5f88
SupriyaBikki <bikki.usedu@gmail.com>
2023-11-28 21:09:52 -0600

    create button added

--- models/employee.js
+++ models/employee.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const employeeSchema = mongoose.Schema({
 employee_type: String,
 department: String,
-salary: Number
+salary: {
+    type : String,
+    min : [1000 , 'minimum 1000'],
+    max : [10000 , 'maximum 10000']
+}
 })
 module.exports = mongoose.model("employee",
 employeeSchema)
