29eecbca35cc1009448bbff85a67a7f5e3511ce5
SupriyaBikki <bikki.usedu@gmail.com>
2023-11-28 21:19:16 -0600

    bonus added

--- routes/resource.js
+++ routes/resource.js
@@ -7,16 +7,22 @@ var router = express.Router();
 var api_controller = require('../controllers/api');
 var employee_controller = require('../controllers/employee');
 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 
 // API ROUTE
 router.get('/', api_controller.api);
 
 // COSTUME ROUTES
-router.post('/employee', employee_controller.employee_create_post);
-router.delete('/employee/:id', employee_controller.employee_delete);
-router.put('/employee/:id', employee_controller.employee_update_put);
-router.get('/employee/:id', employee_controller.employee_detail);
- router.get('/employee', employee_controller.employee_list);
+router.post('/employee', secured, employee_controller.employee_create_post);
+router.delete('/employee/:id',secured, employee_controller.employee_delete);
+router.put('/employee/:id',secured, employee_controller.employee_update_put);
+router.get('/employee/:id',secured, employee_controller.employee_detail);
+ router.get('/employee',secured, employee_controller.employee_list);
 //  router.post('/employees', employee_controller.employee_create_post);
 
 //  router.post('/costumes', costume_controllers.costume_create_post);
